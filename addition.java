8d58dec2cd7b750eac9dab13c00f9cd465ff10b5
Santoshi Joshi <85894419+santoshi1396@users.noreply.github.com>
2021-07-07 19:45:03 +0530

    These are basic programs of java

---
+++ addition.java
@@ -0,0 +1,10 @@
+class Addition{
+
+   public static void main(String[] args) {
+        
+      int  x=10, y=20, sum;
+      sum = x + y;
+
+      System.out.println("Sum of x and y is  "+sum);
+   }
+}	
