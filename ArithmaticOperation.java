15c4f4d292f54adffbeb7fea4c366008ad0b0ad9
Santoshi Joshi <85894419+santoshi1396@users.noreply.github.com>
2021-07-07 20:08:19 +0530

    ArithmaticOperation in java

---
+++ ArithmaticOperation.java
@@ -0,0 +1,16 @@
+class ArithmaticOperation{
+	public static void main(String[] args){
+	int x=50,y=10;
+	System.out.println("Addition of x and y is: "+(x+y));
+
+	System.out.println("Substraction of x and y is: "+(x-y));
+	
+	System.out.println("Multiplication of x and y is: "+(x*y));
+
+	System.out.println("Division of x and y is: "+(x/y));
+		
+	System.out.println("Module of x and y is: "+(x%y));
+	
+	}
+
+}
