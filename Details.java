9affcab938e67823549038b763964476b91e8eea
Santoshi Joshi <85894419+santoshi1396@users.noreply.github.com>
2021-07-07 19:58:49 +0530

    Use of DataType.

---
+++ Details.java
@@ -0,0 +1,15 @@
+class Details{
+	public static void main(String [] args){
+	String studentName="John";
+	int rollNumber=8;
+	final double Percentage=88;
+	char grade='A';
+	
+	System.out.println("Student details:");
+	System.out.println("Student Name: "+studentName +"  RollNumber: "+rollNumber +"  Percentage: "+Percentage+ "  Grade: "+grade);
+	
+	
+	}
+
+
+}
