1bf3d727a121cf0e35f03c2080d6e876891cf6e9
Santoshi Joshi <85894419+santoshi1396@users.noreply.github.com>
2021-07-08 19:42:00 +0530

    Size of Different data types.

---
+++ FundamentalSize.java
@@ -0,0 +1,13 @@
+//Write a program in Java to find Size of fundamental data types.   
+class FundamentalSize{
+	public static void main(String args[]){
+	System.out.println("Size of int: "+(Integer.SIZE/8)+" bytes");
+	System.out.println("Size of Float: "+(Float.SIZE/8)+" bytes");
+	System.out.println("Size of Char: "+(Character.SIZE/8)+" bytes");
+	System.out.println("Size of Double: "+(Double.SIZE/8)+" bytes");
+	//System.out.println("Size of bool: "+(BOOLEAN.SIZE/8)+" bytes");
+	System.out.println("Size of short: "+(Short.SIZE/8)+" bytes");
+
+
+		}
+}
