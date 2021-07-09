f7589779314f32ab24b4fa8752f1fcb579ae496d
Santoshi Joshi <85894419+santoshi1396@users.noreply.github.com>
2021-07-09 14:40:29 +0530

    Assignments

---
+++ AngleofTriangle.java
@@ -0,0 +1,11 @@
+//Write a program in C++ to find the third angle of a triangle.   
+class AngleofTriangle{
+	public static void main(String args[]){
+
+	double a1=30,a2=60,a3;
+	a3=180-(a1+a2);
+	
+	System.out.println("Third angle of Triangle: "+a3);
+	}
+
+}
---
+++ FahrenheittoC.java
@@ -0,0 +1,13 @@
+// Write a program in C++ to convert temperature in Fahrenheit to Celsius.   
+class FahrenheittoC{
+	public static void main(String args[]){
+
+	double f=95,c;
+	c = (f - 32.0) * 5.0 / 9.0;
+	System.out.println("The temperature in Celsius : "+c);
+
+		
+	}
+	
+
+}
